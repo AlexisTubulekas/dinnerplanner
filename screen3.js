040d787866fb98e29b7937de175b6f03ca3b3ce6
AlexisTubulekas <alexistubulekas@gmail.com>
2016-02-16 09:37:59 +0100

    Delete screen3.js

--- screen3.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	//We instantiate our model
-	var model = new DinnerModel();
-	
-	//And create the needed controllers and views
-	var sideBarView = new SideBarView($(".myDinner"), model);
-	var mainView = new MainView($(".images"), model);
-
-});
