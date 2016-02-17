ba969972346e1ccc6b076d454d1f667fe6b87be7
AlexisTubulekas <alexistubulekas@gmail.com>
2016-02-17 13:20:00 +0100

    Final update

--- js/view/mainView.js
+++ js/view/mainView.js
@@ -1,60 +1,25 @@
 //ExampleView Object constructor
 var MainView = function (container,model) {
-	
-	// Get all the relevant elements of the view (ones that show data
-  	// and/or ones that responed to interaction)	
+		
+	this.imageLoop = container.find("#imageLoop");
 
-
-	function LooptyLoop (type) {
+	var imagesStr = "";
+	var loadDishes = function (type) {
 		// body...
 		var typeList = model.getAllDishes(type);
-		var imagesStr = [];
-		for (var i=0; i < 4; i++){
-			imagesStr = imagesStr + '<div class="col-md-1 image">';
-			imagesStr += imagesStr + '<img src="" class="img-thumbnail" alt="Cinque Terre" width="150" height="200"><center>'   ;
-			imagesStr += imagesStr + '<button class="food" data-toggle="collapse" data-target="#demo"><span id=""></span></button>';	 
-			imagesStr += imagesStr + '<div id="demo" class="collapse">Lorem ipsum dolor text....</div></center>'	;
-			imagesStr += imagesStr + '<p class="text-justify description">'
-									+ typeList[i].description + '</p></div>';
-										
-		$("#imageLoop").append(imagesStr);
-		
-		} 
-		
-	}
-
-	LooptyLoop('main dish');
-
-	
-
-	
-}
-
-
- /*
-var ExampleView = function (container, model) {
-	
-	// Get all the relevant elements of the view (ones that show data
-  	// and/or ones that responed to interaction)
-
-
-
-
-	var loadDishes = function (type) {
-		var dishes = model.getAllDishes(type);
-		var dishStr = "";
-		for (var i = 0; i < dishes.length; i++) {
-			var dish = dishes[i];
-			dishStr = '<div class="dishCont" data-id="'+dish.id+'">'; 
-			dishStr += '<div class="turtle"> <img src="images/'+dish.image+'" alt="'+dish.name+'"></img>';
-			dishStr += '<h3 class="dishName">'+dish.name+'</h3> </div>';
-			dishStr += '<div class="description"><h5>'+dish.description+'</h5> </div> </div>';
-
-			$("#dishCont").append(dishStr);
+		for (var i=0; i < typeList.length; i++){ 
+			var dish = typeList[i];
+			imagesStr = imagesStr + '<div class="col-md-2 image">'
+			 + '<center><img src="'+dish.image+'"  alt="'+dish.name+'" width="150" height="200"></img>'
+			 + '<button class="food">'+dish.name+'</button>'	 
+			 + '<p class="text-justify description">'+dish.description+'</p></div></center>';			
 		}
+		return imagesStr;
 	}
 	loadDishes('main dish');
+	this.imageLoop.html(imagesStr);	
 }
 
-*/
+
+
  
