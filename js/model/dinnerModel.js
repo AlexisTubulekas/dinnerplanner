cb044030034ec552050e3861c6e5bfead6373a12
AlexisTubulekas <alexistubulekas@gmail.com>
2016-02-17 13:16:57 +0100

    Final update

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -1,79 +1,102 @@
 //DinnerModel Object constructor
 var DinnerModel = function() {
- 
-	//TODO Lab 2 implement the data structure that will hold number of guest
-	// and selected dinner options for dinner menu
-	numberOfGuests = 1;
-
-	// maybe have
-	//this.menu = new {null, null, null};
-
-	this.setNumberOfGuests = function(num) {
+   var dinnerMenu = [1, 3, 102];
+   var list = [];
+   var otherList = [];
+   var numberOfGuests = 2;
+   
+   
+   	this.setNumberOfGuests = function(num) {
 		//TODO Lab 2
 		//num är värdet av klicket när man ökar/minskar antalet gäster
 		if (num === '1'){
 			numberOfGuests = numberOfGuests + 1;
+            console.log('increase');
 
 		}
 		else if (num == '-1'){
-			numberOfGuests =- numberOfGuests - 1;
+			numberOfGuests = numberOfGuests - 1;
+            console.log('decrease');
 		}
 		return numberOfGuests;	
 		
 	}
 
-	// should return 
 	this.getNumberOfGuests = function() {
 		//TODO Lab 2
 		return numberOfGuests;
 	}
 
-	var dinnerMenu = [1,3,102];
-	//Returns the dish that is on the menu for selected type 
-	//works if there is only 1 of the type you search for
 	this.getSelectedDish = function(type) {
 		var typeList = [];
           for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
             var gottenDish = this.getDish(dinnerMenu[menEl]); //hämtar ut dish om den finns i menyn
             if (gottenDish.type == type){ 
-              typeList.push(gottenDish.name); //lägger i listan om rätt type
+              typeList.push(gottenDish.id); //lägger i listan om rätt type
             }
           }
         return typeList; //skriver ut listan med rätta-typ-rätter
           }
 		//TODO Lab 2
-	
 
-	//Returns all the dishes on the menu.
-	this.getFullMenu = function() {
+
+
+  
+    this.getFullMenu = function() {
 		//TODO Lab 2
 		var fullList = [];
 		for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
             var gottenDish = this.getDish(dinnerMenu[menEl]);
-            fullList.push(gottenDish.name);
+            fullList.push(gottenDish);
         	}
-        return fullList; //skriver ut listan av alla rätter i menyn
+        return fullList; //skriver ut listan med rätta-typ-rätter
           }
-	
-
-	//Returns all ingredients for all the dishes on the menu.
+    
+  
     this.getAllIngredients = function() {
 		//TODO Lab 2
 		var ingrList = [];	
-      	for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
+      for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
             var gottenDish = this.getDish(dinnerMenu[menEl]);
-            var gottenIngr = gottenDish.ingredients; //lista av ingredient objekt
+            var gottenIngr = gottenDish.ingredients;
           for (var key in gottenIngr){
-            ingrList.push(gottenIngr[key].name);
+            ingrList.push(gottenIngr[key]);
           }
         	}
         return ingrList; //skriver ut listan med rätta-typ-rätter
           }
-	
 
+
+    this.getDishPrice = function(id){
+
+    	var priceList = [];	
+    	var gottenDish = this.getDish(id);
+    	var gottenIngr = gottenDish.ingredients;
+    	for (var key in gottenIngr){
+    		//console.log(gottenIngr[key].price);
+            priceList.push(gottenIngr[key].price);
+          }
+       	var sum = 0;
+       	var len = priceList.length;
+      	for (var i=0; i < len; i++){
+      		//console.log('listan är '+ priceList.length);
+      		var popped = priceList.pop();
+      		sum = sum + popped;
+      		//console.log('summan är' + sum);
+      	}
+      	var sum = sum*numberOfGuests; 
+      	if (isNaN(sum) === true){
+         	console.log('nan');
+         	return ('0.00');
+         }
+         else if (isNaN(sum) === false){
+         	console.log('all good sum!' + sum);
+         	return (sum+'.00');
+         }
+    }
 //Returns the total price of the menu (all the ingredients multiplied by number of guests).
 
- this.getTotalMenuPrice = function() {
+ 	this.getTotalMenuPrice = function() {
 		//TODO Lab 2
 		var priceList = [];	
       for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
@@ -83,40 +106,57 @@ var DinnerModel = function() {
             priceList.push(gottenIngr[key].price);
           }
         	}
-         var sum = priceList.reduce(function(pv, cv) { return pv + cv; }, 0);
+        	
+      var sum = 0;
+      var len = priceList.length;
+      for (i=0; i < len; i++){
+      		var popped = priceList.pop();
+      		var sum = sum + popped;
+      }  
+         //var sum = priceList.reduce(function(pv, cv) { return pv + cv; }, 0);
          var sum = sum*numberOfGuests;
-        return ('total price:' + sum); //skriver ut listan med rätta-typ-rätter
+         if (isNaN(sum) === true){
+         	console.log('nan');
+         	return ('0.00');
+         }
+         else if (isNaN(sum) === false){
+         	console.log('all good sum!' + sum);
+         	return (sum+'.00');
+         }
+         //skriver ut listan med rätta-typ-rätter
           }
-
-	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
+	
+  	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
 	//it is removed from the menu and the new one added.
 	this.addDishToMenu = function(id) {
 		//TODO Lab 2 
       var dishToAdd = this.getDish(id);
       for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
-	      var gottenDish = this.getDish(dinnerMenu[menEl]); //hämtar ut dish om den finns i menyn
-	      if (gottenDish.type == dishToAdd.type){
-	        dinnerMenu.splice(menEl, 1, dishToAdd.id);       
-	      }
-	      else if (gottenDish.id in dinnerMenu){
-	        dinnerMenu.push(dishToAdd.id);
-	      }
+      var gottenDish = this.getDish(dinnerMenu[menEl]); //hämtar ut dish om den finns i menyn
+      if (gottenDish.type == dishToAdd.type){
+        dinnerMenu.splice(menEl, 1, dishToAdd.id);       
+      }
+      else if (gottenDish.id in dinnerMenu){
+        dinnerMenu.push(dishToAdd.id);
+      }
       }
       return dinnerMenu;
-	};
-
-	//Removes dish from menu
-	this.removeDishFromMenu = function(id) {
+	}
+  
+this.removeDishFromMenu = function(id) {
 		//TODO Lab 2
-		var dishToRemove = getDish(id);
+		var dishToRemove = this.getDish(id);
 		for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
             var gottenDish = this.getDish(dinnerMenu[menEl]);
             if (dishToRemove.id == gottenDish.id){
             	dinnerMenu.splice(menEl, 1);
             }
-	}
+	    }
+        return dinnerMenu;
+    };
+    
 
-	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
+  //function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
 	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
 	//if you don't pass any filter all the dishes will be returned
 	this.getAllDishes = function (type,filter) {
@@ -136,16 +176,30 @@ var DinnerModel = function() {
 		}
 	  	return dish.type == type && found;
 	  });	
-	}
-
-	//function that returns a dish of specific ID
+	} 
+	
+  //function that returns a dish of specific ID
 	this.getDish = function (id) {
-	  for(key in dishes){
+	  for(var key in dishes){
 			if(dishes[key].id == id) {
 				return dishes[key];
 			}
 		}
-	}
+	};
+  
+
+
+   /* this.getIngredient = function(id){
+		for (menEl = 0; menEl< dinnerMenu.length; menEl++){ //går igenom varje element i menyn
+            var gottenDish = this.getDish(dinnerMenu[menEl]);
+				gottenDish.ingredients.forEach ();
+                  //console.log('ob is ' + ob);
+					//return dishes[ob].ingredients;
+				}
+			};*/
+		
+	
+    
 
 	// the dishes variable contains an array of all the 
 	// dishes in the database. each dish has id, name, type,
